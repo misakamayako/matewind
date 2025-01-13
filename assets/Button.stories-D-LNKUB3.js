078fc675fa4592c7611483b924f406e2ae8f34b7
misakamayako <514183295@qq.com>
2025-01-13 02:41:31 +0000

    Deploying to gh-pages from @ misakamayako/matewind@6e337537f62b2d14824e8ba9c39b875cd8514209 🚀

---
+++ assets/Button.stories-D-LNKUB3.js
@@ -0,0 +1,62 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{f as d}from"./index-Dj6nxAlZ.js";import{B as i}from"./Button-CuA4Qlat.js";import"./index-2yJIXLcc.js";const f={component:i},e={args:{children:r.jsx("span",{children:"click me"}),onClick:d(()=>{console.log(1)})},argTypes:{children:{table:{disable:!0}},className:{table:{disable:!0}},loading:{table:{type:{summary:"React.ReactNode"},description:"React.ReactNode"},type:"boolean",defaultValue:!1}}},a={args:{children:r.jsx("span",{children:"click me"}),onClick:d(()=>{console.log(1)}),disabled:!1,loading:!1,type:"cancel"},argTypes:{children:{table:{disable:!0}},className:{table:{disable:!0}},loading:{table:{type:{summary:"React.ReactNode"},description:"React.ReactNode"},type:"boolean",defaultValue:!1}}};var n,t,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: <span>click me</span>,
+    onClick: fn(() => {
+      console.log(1);
+    })
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    },
+    className: {
+      table: {
+        disable: true
+      }
+    },
+    loading: {
+      table: {
+        type: {
+          summary: "React.ReactNode"
+        },
+        description: "React.ReactNode"
+      },
+      type: "boolean",
+      defaultValue: false
+    }
+  }
+}`,...(l=(t=e.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};var s,o,c;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: <span>click me</span>,
+    onClick: fn(() => {
+      console.log(1);
+    }),
+    disabled: false,
+    loading: false,
+    type: "cancel"
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    },
+    className: {
+      table: {
+        disable: true
+      }
+    },
+    loading: {
+      table: {
+        type: {
+          summary: "React.ReactNode"
+        },
+        description: "React.ReactNode"
+      },
+      type: "boolean",
+      defaultValue: false
+    }
+  }
+}`,...(c=(o=a.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const g=["Default","GhostButton"];export{e as Default,a as GhostButton,g as __namedExportsOrder,f as default};
---
+++ assets/Modal.stories-CJiPArND.js
@@ -0,0 +1,10 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{f as n}from"./index-Dj6nxAlZ.js";import{M as i}from"./Modal-CJPQmxAV.js";import"./Button-CuA4Qlat.js";import"./index-2yJIXLcc.js";import"./transition-DboQz4Id.js";import"./index-D7IJEsYv.js";import"./index-Bn05qqr6.js";const h={component:i},t={args:{open:!0,children:s.jsx("div",{children:"this is content"}),onClose:n(),showCancel:!0,showConfirm:!0,title:"this is title"}};var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    open: true,
+    children: <div>this is content</div>,
+    onClose: fn(),
+    showCancel: true,
+    showConfirm: true,
+    title: "this is title"
+  }
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const C=["Default"];export{t as Default,C as __namedExportsOrder,h as default};
