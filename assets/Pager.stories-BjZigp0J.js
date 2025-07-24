28c78cf2fa5f102764a49ea368b847dc9d124af7
misakamayako <514183295@qq.com>
2025-07-24 07:59:06 +0000

    Deploying to gh-pages from @ misakamayako/matewind@316ac60313e9118320548c235fe68d188eeb00b9 🚀

---
+++ assets/Pager.stories-BjZigp0J.js
@@ -0,0 +1,9 @@
+import{f as t}from"./index-BEO02Wfb.js";import{P as n}from"./Pager-DU_CWUze.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import"./Button-Du0jJBL6.js";const i={component:n},r={args:{total:500,pageSize:10,current:5,onChange:t()},argTypes:{}};var e,a,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    total: 500,
+    pageSize: 10,
+    current: 5,
+    onChange: fn()
+  },
+  argTypes: {}
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,i as default};
