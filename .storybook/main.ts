099b739045871612e09294a9f9c0bc596c53505b
misaka <514183295@qq.com>
2026-07-27 10:52:16 +0800

    更新版本

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,28 +3,22 @@ import type {StorybookConfig} from "@storybook/react-vite";
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
-    addons: [
-        "@storybook/addon-essentials",
-        "@chromatic-com/storybook",
-        "@storybook/addon-interactions",
-        {
-			name: '@storybook/addon-styling-webpack',
-			options: {
-				rules: [
-					{
-						test: /\.less$/,
-						use: [
-							'style-loader',
-							'css-loader',
-							'postcss-loader',  // 添加PostCSS支持
-							'less-loader',  // Add this for LESS support
-						],
-					},
-				],
-			},
-		},
-        "@storybook/addon-mdx-gfm"
-    ],
+    addons: ["@chromatic-com/storybook", {
+        name: '@storybook/addon-styling-webpack',
+        options: {
+            rules: [
+                {
+                    test: /\.less$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'postcss-loader',  // 添加PostCSS支持
+                        'less-loader',  // Add this for LESS support
+                    ],
+                },
+            ],
+        },
+    }, '@storybook/addon-docs'],
 
     framework: {
 		name: "@storybook/react-vite",
